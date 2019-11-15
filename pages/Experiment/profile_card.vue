104866627d2e5f728c533762bff7835793c9e7e4
ZhangYuS <834544739@qq.com>
2019-11-15 13:58:34 +0800

    创建 profile_card 组件

---
+++ pages/Experiment/profile_card.vue
@@ -0,0 +1,24 @@
+<template>
+  <Card style="width:320px">
+    <div style="text-align:center">
+      <img src="~/assets/default.jpg">
+      <h3>A high quality UI Toolkit based on Vue.js</h3>
+    </div>
+  </Card>
+</template>
+
+<script>
+export default {
+  name: 'profile_card',
+  data () {
+    return {
+      avatar: '~/assets/default.jpg',
+      username: '游客'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
