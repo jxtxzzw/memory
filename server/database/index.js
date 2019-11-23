2f5e004300de29fb2c076a7d34a980916a1a08d1
jxtxzzw <jxtxzzw@126.com>
2019-11-23 23:14:32 +0800

    配置数据库的生产环境
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- server/database/index.js
+++ server/database/index.js
@@ -1,10 +1,23 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(
-  {
-    dialect: 'sqlite',
-    storage: 'dev_db.sqlite'
-  }
-)
+let sequelize
+
+if (process.env.MEMORY_PROD) {
+  sequelize = new Sequelize(
+    process.env.MEMORY_DB,
+    process.env.MEMORY_USERNAME,
+    process.env.MEMORY_PASSWORD,
+    {
+      dialect: 'mariadb'
+    }
+  )
+} else {
+  sequelize = new Sequelize(
+    {
+      dialect: 'sqlite',
+      storage: 'dev_db.sqlite'
+    }
+  )
+}
 
 module.exports = sequelize
