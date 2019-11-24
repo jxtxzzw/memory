ee1e4ff1e9d7b3277f256459360a1d4cc03b1652
jxtxzzw <jxtxzzw@126.com>
2019-11-24 23:20:00 +0800

    初步尝试 Scroll 的布局分布
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- pages/Experiment/Scroll.vue
+++ pages/Experiment/Scroll.vue
@@ -1,13 +1,29 @@
 <template>
-  <div style="height: 600px">
-    {{ screenHeight }}
-    <Scroll :on-reach-bottom="handleReachBottom" :height="screenHeight">
-      <Row>
-        <i-col v-for="(item, index) in list1" :key="index" span="6">
-          <Card style="margin: 32px 0">
+  <div>
+    <Scroll
+      :on-reach-bottom="handleReachBottom"
+      loading-text="加载中……"
+      :height="scrollHeight"
+    >
+      {{ itemNumberPerLine }} <br>
+      {{ itemSpanInLastLine }}
+      <Row type="flex" justify="center" :gutter="itemSpan">
+        <i-col v-for="(item, index) in crowdList" :key="index">
+          <Card style="margin: 32px 0; width: 320px">
             <div style="text-align:center">
               <img src="https://file.iviewui.com/dist/2ecd3b0452aa197097d5131afacab7b8.png">
-              <h3>这是第 {{ index }} 本书</h3>
+              <h3>这是第 {{ index + 1 }} 本书</h3>
+            </div>
+          </Card>
+        </i-col>
+      </Row>
+      <Row type="flex" justify="start" :gutter="itemSpan">
+        <div :style="{width: itemSpanInLastLine + 'px'}"></div>
+        <i-col v-for="(item, index) in lastLineList" :key="index">
+          <Card style="margin: 32px 0; width: 320px">
+            <div style="text-align:center">
+              <img src="https://file.iviewui.com/dist/2ecd3b0452aa197097d5131afacab7b8.png">
+              <h3>这是第 {{ index + itemIndexInLastLine + 1 }} 本书</h3>
             </div>
           </Card>
         </i-col>
@@ -19,8 +35,37 @@
 export default {
   data () {
     return {
-      list1: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-      screenHeight: 1000
+      itemSpan: 70,
+      itemWidth: 320,
+      itemHeight: 320,
+      list1: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+      screenHeight: 1000,
+      screenWidth: 1000
+    }
+  },
+  computed: {
+    scrollHeight () {
+      const layoutHeight = this.screenHeight > 170 ? this.screenHeight - 170 : this.screenHeight
+      const itemHeight = this.list1.length / 4 * 300
+      return itemHeight < layoutHeight ? itemHeight : layoutHeight
+    },
+    layoutWidth () {
+      return this.screenWidth - 100 - 1.5
+    },
+    itemNumberPerLine () {
+      return Math.floor(this.layoutWidth / (this.itemWidth + this.itemSpan))
+    },
+    itemIndexInLastLine () {
+      return this.list1.length - this.list1.length % this.itemNumberPerLine
+    },
+    crowdList () {
+      return this.list1.slice(0, this.itemIndexInLastLine)
+    },
+    lastLineList () {
+      return this.list1.slice(this.itemIndexInLastLine)
+    },
+    itemSpanInLastLine () {
+      return (this.layoutWidth - this.itemNumberPerLine * this.itemWidth - (this.itemNumberPerLine - 1) * this.itemSpan) / 2 - this.itemSpan / 2
     }
   },
   watch: {
@@ -34,14 +79,33 @@ export default {
           that.timer = false
         }, 400)
       }
+    },
+    screenWidth (val) {
+      if (!this.timer) {
+        this.screenWidth = val
+        this.timer = true
+        const that = this
+        setTimeout(function () {
+          that.init()
+          that.timer = false
+        }, 400)
+      }
     }
   },
   mounted () {
+    this.screenHeight = document.body.clientHeight
+    this.screenWidth = document.body.clientWidth
     const that = this
     window.onresize = () => {
       return (() => {
         window.screenHeight = document.body.clientHeight
         that.screenHeight = window.screenHeight
+      })()
+    }
+    window.onresize = () => {
+      return (() => {
+        window.screenWidth = document.body.clientWidth
+        that.screenWidth = window.screenWidth
       })()
     }
   },
