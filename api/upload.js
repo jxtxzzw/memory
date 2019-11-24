43799821b1835199529fe5fef95b159f90d97049
SYRain <834544739@qq.com>
2019-11-24 22:58:45 +0800

    完成添加功能

--- api/upload.js
+++ api/upload.js
@@ -1,14 +1,51 @@
+// eslint-disable-next-line no-unused-vars
 const fs = require('fs')
+const typeList = require('../assets/mimetype')
+const Item = require('../server/database/models/Item')
+const ItemCategory = require('../server/database/models/ItemCategory')
+const Tag = require('../server/database/models/Tag')
+const ItemTag = require('../server/database/models/ItemTag')
 const router = require('./router')
-router.post('/upload', (req, res, next) => {
-  const imgData = req.body.file.thumbUrl
-  const base64Data = imgData.replace(/^data:image\/\w+;base64,/, '')
-  const dataBuffer = Buffer.from(base64Data, 'base64')
-  fs.writeFile('image.png', dataBuffer, function (err) {
-    if (err) {
-      res.send(err)
-    } else {
-      res.send('保存成功！')
+// eslint-disable-next-line no-unused-vars
+
+async function CreateItem (data) {
+  try {
+    const item = Item.build({})
+    item.title = data.title
+    item.type = data.type
+    await item.save()
+    const imgData = data.fileList[0].thumbUrl
+    const base64Data = imgData.replace(/(.*)?;base64,/, '')
+    const dataBuffer = Buffer.from(base64Data, 'base64')
+    item.cover = item.id + '.' + typeList[data.fileList[0].type]
+    await item.save()
+    fs.writeFile('./static/upload' + item.cover, dataBuffer)
+    for (const category of data.checkedCategory) {
+      await ItemCategory.create({ item: item.id, category: category.id })
     }
-  })
+    for (const tag of data.tags) {
+      const [tagInstance] = await Tag.findOrCreate({
+        where: {
+          name: tag
+        },
+        defaults: {
+          name: tag
+        }
+      })
+      ItemTag.create({ item: item.id, tag: tagInstance.id })
+    }
+  } catch (e) {
+    console.log(e.message)
+    throw new Error('创建失败')
+  }
+}
+
+router.post('/upload', async (req, res, next) => {
+  try {
+    await CreateItem(req.body)
+    console.log('++++++++++++++++++++++++++++++++++++++++++++++')
+    res.status(200).json('创建成功')
+  } catch (e) {
+    res.status(400).json(e.message)
+  }
 })
