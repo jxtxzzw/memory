0f0d07bf5363afae59bdf6e973a651303c529f2c
SYRain <834544739@qq.com>
2019-11-25 20:53:13 +0800

    未完成的 hook

--- server/database/models/Rating.js
+++ server/database/models/Rating.js
@@ -36,8 +36,31 @@ Rating.init({
     type: Sequelize.STRING,
     allowNull: true
   }
-}, {
+}, { hooks: {
+  afterCreate (instance, options) {
+    Rating.findAll({
+      where: {
+        item: instance.item
+      },
+      attributes: [Model.sequelize.fn('AVG', Model.sequelize.col('rating'), 'avgRating')]
+    }).then((projects) => {
+      console.log(projects)
+    })
+  },
+  afterUpdate (instance, options) {
+    Rating.findAll({
+      where: {
+        item: instance.item
+      },
+      attributes: [Model.sequelize.fn('AVG', Model.sequelize.col('rating'), 'avgRating')]
+    }).then((projects) => {
+      console.log(projects)
+    })
+  }
+} }, {
   sequelize
 })
+
+// TODO 未完成的 hooks，需要先写完其他接口进行测试
 
 module.exports = Rating
