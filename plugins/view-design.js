6f9e0818f1e259edc8c58283b3208d83b93276a1
jxtxzzw <jxtxzzw@126.com>
2019-11-27 00:14:31 +0800

    修改 iView 默认 locale 为 zh-CN
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- plugins/view-design.js
+++ plugins/view-design.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import ViewUI from 'view-design'
-import locale from 'view-design/dist/locale/en-US' // Change locale, check node_modules/view-design/dist/locale
+import locale from 'view-design/dist/locale/zh-CN' // Change locale, check node_modules/view-design/dist/locale
 
 Vue.use(ViewUI, {
   locale
