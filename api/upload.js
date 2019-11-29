ca4ebd678ac18c437e32fea67ed84be67028c4e6
SYRain <834544739@qq.com>
2019-11-29 17:17:52 +0800

    增加修改功能

--- api/upload.js
+++ api/upload.js
@@ -11,22 +11,48 @@ const router = require('./router')
 async function CreateItem (data) {
   try {
     let cover
-    if (Object.prototype.hasOwnProperty.call(typeList, data.fileList[0].type)) {
-      const imgData = data.fileList[0].thumbUrl
-      const base64Data = imgData.replace(/(.*)?;base64,/, '')
-      const dataBuffer = Buffer.from(base64Data, 'base64')
-      cover = new Date().getTime() + '.' + typeList[data.fileList[0].type]
-      fs.writeFile('./static/upload/' + cover, dataBuffer, () => {})
-    } else {
-      throw new Error('图片类型不符合')
+    if (data.fileList[0] && data.fileList[0].thumbUrl) {
+      if (Object.prototype.hasOwnProperty.call(typeList, data.fileList[0].type)) {
+        const imgData = data.fileList[0].thumbUrl
+        const base64Data = imgData.replace(/(.*)?;base64,/, '')
+        const dataBuffer = Buffer.from(base64Data, 'base64')
+        cover = new Date().getTime() + '.' + typeList[data.fileList[0].type]
+        fs.writeFile('./static/upload/' + cover, dataBuffer, () => {
+        })
+      } else {
+        throw new Error('图片类型不符合')
+      }
     }
-    const item = Item.build({})
-    item.title = data.title
-    item.type = data.type
-    item.cover = cover
-    await item.save()
+    const [instance, created] = await Item.findOrCreate({
+      where: {
+        id: data.id || 0
+      },
+      defaults: {
+        title: data.title,
+        type: data.type,
+        cover
+      }
+    })
+    if (!created) {
+      instance.title = data.title
+      instance.type = data.type
+      if (cover) {
+        instance.cover = cover
+      }
+      instance.save()
+    }
+    ItemCategory.destroy({
+      where: {
+        item: instance.id
+      }
+    })
+    ItemTag.destroy({
+      where: {
+        item: instance.id
+      }
+    })
     for (const category of data.checkedCategory) {
-      await ItemCategory.create({ item: item.id, category: category.id })
+      await ItemCategory.create({ item: instance.id, category: category.id })
     }
     for (const tag of data.tags) {
       const [tagInstance] = await Tag.findOrCreate({
@@ -37,17 +63,17 @@ async function CreateItem (data) {
           name: tag
         }
       })
-      ItemTag.create({ item: item.id, tag: tagInstance.id })
+      ItemTag.create({ item: instance.id, tag: tagInstance.id })
     }
   } catch (e) {
-    throw new Error('创建失败:' + e)
+    throw new Error('失败:' + e)
   }
 }
 
 router.post('/upload', async (req, res, next) => {
   try {
     await CreateItem(req.body)
-    res.status(200).json('创建成功')
+    res.status(200).json('成功')
   } catch (e) {
     res.status(400).json(e.message)
   }
