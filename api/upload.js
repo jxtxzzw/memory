69f61d63aa45f0c46d53a3f7366837f1cbc4b869
jxtxzzw <jxtxzzw@126.com>
2019-11-30 15:45:45 +0800

    增加 creator 和 updater
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- api/upload.js
+++ api/upload.js
@@ -65,7 +65,7 @@ async function CreateItem (data) {
       })
       ItemTag.create({ item: instance.id, tag: tagInstance.id })
     }
-    return instance.id
+    return instance
   } catch (e) {
     throw new Error('失败:' + e)
   }
@@ -73,9 +73,14 @@ async function CreateItem (data) {
 
 router.post('/upload', async (req, res, next) => {
   try {
-    const id = await CreateItem(req.body)
+    const item = await CreateItem(req.body)
+    if (!item.creator) {
+      item.creator = req.user.id
+    }
+    item.updater = req.user.id
+    item.save()
     res.status(200).json({
-      id
+      id: item.id
     })
   } catch (e) {
     res.status(400).json(e.message)
