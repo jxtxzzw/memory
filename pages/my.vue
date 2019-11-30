1f1d420e44577426e602835a8921dd2391abe3b3
jxtxzzw <jxtxzzw@126.com>
2019-11-30 14:34:06 +0800

    个人主页完成
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- pages/my.vue
+++ pages/my.vue
@@ -1,10 +1,26 @@
 <template>
   <div>
     <Row>
-      <i-col span="12">
+      <i-col span="10">
+        <Divider>修改头像</Divider>
+        <Alert show-icon>
+          修改您的头像
+          <span slot="desc">
+            上传一张图片来修改您的头像。留空并直接点击“上传”表示使用默认头像。您可能需要重新登录才能看到新的头像。
+          </span>
+        </Alert>
         <ChangeAvatar />
+        <Divider>修改密码</Divider>
+        <Alert show-icon>
+          修改您的密码
+          <span slot="desc">
+            在这里修改您的密码
+          </span>
+        </Alert>
+        <ChangePassword />
       </i-col>
-      <i-col offset="12" span="12">
+      <i-col offset="2" span="10" style="overflow-y: auto">
+        <Divider>我的动态</Divider>
         <MyActivity />
       </i-col>
     </Row>
@@ -14,10 +30,11 @@
 <script>
 import MyActivity from '../components/MyActivity'
 import ChangeAvatar from '../components/ChangeAvatar'
+import ChangePassword from '../components/ChangePassword'
 export default {
   middleware: ['auth'],
   name: 'My',
-  components: { ChangeAvatar, MyActivity }
+  components: { ChangePassword, ChangeAvatar, MyActivity }
 }
 </script>
 
