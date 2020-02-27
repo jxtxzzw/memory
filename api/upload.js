c262b52461f9a2753b090bb23cf55f818c8b95af
jxtxzzw <jxtxzzw@126.com>
2020-02-27 21:04:59 +0800

    修复：Item 的 title, note 等修改后无法保存的 bug（由于事务引起）
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- api/upload.js
+++ api/upload.js
@@ -35,8 +35,12 @@ async function CreateItem (data) {
         type: data.type,
         cover,
         note: data.note
-      },
-      transaction
+      }
+      // 因为 instance.save(transaction) 在下面已经有了事务，所以 findOrCreate 这里不需要事务了
+      // 否则，如果这里也加了事务，就只有在 orCreate 时才能成功（因为没进 if (!created)）
+      // 或者，find 后只有仅仅修改了 tag/category 时才能正常完成（因为进了 if (!created) 后直接什么都没修改就 save instance 了）
+      // find 后如果修改了 title/note 等，instance.save(transaction) 就会和这里的 transaction 冲突
+      // 所以 transaction 放在下面，这里不需要了
     })
     if (!created) {
       instance.title = data.title
@@ -45,8 +49,10 @@ async function CreateItem (data) {
       if (cover) {
         instance.cover = cover
       }
+      // 对 Item 的事务只在这里有
       await instance.save(transaction)
     }
+    // Category 和 Tag 走联系集，与上面无关，照常 transaction
     await ItemCategory.destroy({
       where: {
         item: instance.id
