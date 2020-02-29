60c177fb54d3ad240b5018f7f3d69f34cd1cc7ac
jxtxzzw <jxtxzzw@126.com>
2020-02-29 22:43:02 +0800

    动态区域增加滚动条
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- pages/activity.vue
+++ pages/activity.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div :style="{height: cardInnerHeight + 'px', overflowY: 'auto'}">
     <MyActivity :user="undefined" />
   </div>
 </template>
@@ -9,7 +9,41 @@ import MyActivity from '../components/MyActivity'
 export default {
   middleware: ['auth'],
   name: 'My',
-  components: { MyActivity }
+  components: { MyActivity },
+  data () {
+    return {
+      screenHeight: 0
+    }
+  },
+  computed: {
+    cardInnerHeight () {
+      // cardHeight 与 default layout 计算类似，为屏幕高度减去 137，之后需要再减去 2 个 16 的 padding
+      return this.screenHeight - 137 - 32
+    }
+  },
+  mounted () {
+    this.addWindowOnResizeHook()
+  },
+  methods: {
+    addWindowOnResizeHook () {
+      // 找到根元素下面谁是 default layout
+      let defaultLayoutVue
+      for (let i = 0; i < this.$root.$children.length; i++) {
+        if (this.$root.$children[i].onResizeHook) {
+          defaultLayoutVue = this.$root.$children[i]
+          break
+        }
+      }
+      // 将 default layout 的 screenHeight 留下来，作为初始值
+      this.screenHeight = document.body.clientHeight
+      this.screenWidth = document.body.clientWidth
+      // 在 default layout 的 onResizeHook 中添加一个函数，表示窗口大小变化时，需要更新该组件的 screenHeight 为 default layout 的 screenHeight
+      defaultLayoutVue.onResizeHook.push(() => {
+        this.screenHeight = document.body.clientHeight
+        this.screenWidth = document.body.clientWidth
+      })
+    }
+  }
 }
 </script>
 
--- pages/user/_id.vue
+++ pages/user/_id.vue
@@ -6,7 +6,9 @@
       </Col>
       <Col span="16">
       <Divider> {{ username }} 的动态 </Divider>
-      <MyActivity :user="parseInt($route.params.id)" />
+      <div :style="{height: cardInnerHeight + 'px', overflowY: 'auto'}">
+        <MyActivity :user="parseInt($route.params.id)" />
+      </div>
       </Col>
     </Row>
   </div>
@@ -25,10 +27,19 @@ export default {
     return {
       username: '',
       avatar: '',
-      id: ''
+      id: '',
+      screenHeight: 0
+    }
+  },
+  computed: {
+    cardInnerHeight () {
+      // cardHeight 与 default layout 计算类似，为屏幕高度减去 137，之后需要再减去 2 个 16 的 padding
+      // 这里还要减去 Divider 的 24 以及它的 2 个 margin
+      return this.screenHeight - 137 - 32 - (24 + 16 + 16)
     }
   },
   async mounted () {
+    this.addWindowOnResizeHook()
     await this.loadData()
   },
   methods: {
@@ -39,6 +50,24 @@ export default {
       this.username = user.username
       this.avatar = user.avatar
       this.id = user.id
+    },
+    addWindowOnResizeHook () {
+      // 找到根元素下面谁是 default layout
+      let defaultLayoutVue
+      for (let i = 0; i < this.$root.$children.length; i++) {
+        if (this.$root.$children[i].onResizeHook) {
+          defaultLayoutVue = this.$root.$children[i]
+          break
+        }
+      }
+      // 将 default layout 的 screenHeight 留下来，作为初始值
+      this.screenHeight = document.body.clientHeight
+      this.screenWidth = document.body.clientWidth
+      // 在 default layout 的 onResizeHook 中添加一个函数，表示窗口大小变化时，需要更新该组件的 screenHeight 为 default layout 的 screenHeight
+      defaultLayoutVue.onResizeHook.push(() => {
+        this.screenHeight = document.body.clientHeight
+        this.screenWidth = document.body.clientWidth
+      })
     }
   }
 }
